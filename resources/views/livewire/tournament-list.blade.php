7f63b42b4d417fd631d63396dc33c2804fab1090
bthouverez <bastien.thouverez@gmail.com>
2023-11-15 00:51:04 +0100

    radio component

--- resources/views/livewire/tournament-list.blade.php
+++ resources/views/livewire/tournament-list.blade.php
@@ -1,87 +1,32 @@
 <div class="flex justify-around m-6 flex-wrap md:flex-nowrap gap-4">
-    <div class="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h1 class="text-xl font-bold mb-3 dark:text-white">{{ __('My tournaments') }}</h1>
-        <div class="flex gap-3 flex-wrap">
-            @forelse($tournaments->sortByDesc('date') as $tournament)
-                <a href="/tournaments/{{ $tournament->id }}/registration"
-                   class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-200">{{ $tournament->label }}</h5>
-                    <div class="font-normal text-gray-700 dark:text-gray-400">
-                        <p>{{ Carbon\Carbon::parse($tournament->date)->format('j M Y') }}</p>
-                        <p>{{ $tournament->date == date('Y-m-d') ?
-                            __('Today') : Carbon\Carbon::parse($tournament->date)->diffForHumans() }}</p>
-                        <p>{{ $tournament->place }}</p>
-                        <p>
-                        @if($tournament->team_size == 0)
-                            {{ __('Precision') }}
-                        @else
-                            @if($tournament->team_size == 1)
-                                {{ __('Simple') }}
-                            @elseif($tournament->team_size == 2)
-                                {{ __('Double') }}
-                            @elseif($tournament->team_size == 3)
-                                {{ __('Triple') }}
-                            @endif
-                            -
-                            @if($tournament->has_brackets)
-                                {{ __('Pools') }}
-                            @else
-                                {{ __('Playoff') }}
-                            @endif
-                        @endif
-                        </p>
-                    </div>
-                </a>
-            @empty
-                <p class=" dark:text-gray-300">{{ __('No tournament registered') }}</p>
-            @endforelse
-        </div>
-    </div>
-    <form wire:submit="createTournament" class="grow bg-white dark:bg-gray-800  shadow-md rounded px-8 pt-6 pb-8 mb-4 min-w-42">
+    <form wire:submit="createTournament"
+          class="bg-white dark:bg-gray-800  shadow-md rounded px-8 pt-6 pb-8 mb-4 grow-0 w-80 min-w-max">
         <h1 class="text-xl font-bold dark:text-gray-300">{{ __('Create new tournament') }}</h1>
         <div class="my-4">
             <p class="font-bold p-2 dark:text-gray-300">{{ __('Type') }}</p>
             @error('team_size') <p class="text-red-500">{{ $message }}</p> @enderror
             <ul class="grid md:grid-cols-2 text-center">
                 <li>
-                    <input type="radio" id="precision" name="team_size" value="precision" class="hidden peer" required>
-                    <label wire:click="setTeamSize(0)" for="precision" class="p-2
-                    inline-flex items-center justify-between w-full text-gray-500 bg-white border border-gray-500 cursor-pointer
-                    peer-checked:border-blue-600 peer-checked:text-white  peer-checked:bg-blue-500  hover:bg-gray-100
-                     dark:bg-gray-800 dark:hover:bg-gray-700 rounded-tl-lg
-                     ">
-                        <div class="w-full ">{{ __('Precision') }}</div>
-                    </label>
+                    <x-radio wire:click="setTeamSize(0)" value="precision" name="team_size"
+                             class="rounded-t-lg md:rounded-tr-none">
+                        {{ __('Precision') }}
+                    </x-radio>
                 </li>
                 <li>
-                    <input type="radio" id="simple" name="team_size" value="simple" class="hidden peer" required>
-                    <label wire:click="setTeamSize(1)" for="simple" class="p-2
-                    inline-flex items-center justify-between w-full text-gray-500 bg-white border border-gray-500 cursor-pointer
-                    peer-checked:border-blue-600 peer-checked:text-white  peer-checked:bg-blue-500  hover:bg-gray-100
-                     dark:bg-gray-800 dark:hover:bg-gray-700 rounded-tr-lg
-                     ">
-                        <div class="w-full truncate">{{ __('Simple') }}</div>
-                    </label>
+                    <x-radio wire:click="setTeamSize(1)" value="simple" name="team_size" class="md:rounded-tr-lg">
+                        {{ __('Simple') }}
+                    </x-radio>
                 </li>
                 <li>
-                    <input type="radio" id="double" name="team_size" value="double" class="hidden peer">
-                    <label wire:click="setTeamSize(2)" for="double" class="p-2
-                    inline-flex items-center justify-between w-full text-gray-500 bg-white border border-gray-500 cursor-pointer
-                    peer-checked:border-blue-600 peer-checked:text-white  peer-checked:bg-blue-500 hover:bg-gray-100
-                     dark:bg-gray-800 dark:hover:bg-gray-700 rounded-bl-lg
-                     ">
-                        <div class="w-full truncate">{{ __('Double') }}</div>
-                    </label>
+                    <x-radio wire:click="setTeamSize(2)" value="double" name="team_size" class="md:rounded-bl-lg">
+                        {{ __('Double') }}
+                    </x-radio>
                 </li>
                 <li>
-                    <input type="radio" id="triple" name="team_size" value="triple" class="hidden peer">
-                    <label wire:click="setTeamSize(3)" for="triple" class="p-2
-                    inline-flex items-center justify-between w-full text-gray-500 bg-white border border-gray-500 cursor-pointer
-                    peer-checked:border-blue-600 peer-checked:text-white  peer-checked:bg-blue-500 hover:bg-gray-100
-                     dark:bg-gray-800 dark:hover:bg-gray-700 rounded-br-lg
-                     ">
-                        <div class="w-full truncate">{{ __('Triple') }}</div>
-                    </label>
+                    <x-radio wire:click="setTeamSize(3)" value="triple" name="team_size"
+                             class="rounded-b-lg md:rounded-bl-none">
+                        {{ __('Triple') }}
+                    </x-radio>
                 </li>
             </ul>
         </div>
@@ -90,58 +35,81 @@
             @error('has_brackets') <p class="text-red-500">{{ $message }}</p> @enderror
             <ul class="grid w-full md:grid-cols-2 text-center">
                 <li>
-                    <input type="radio" id="bracket" name="has_brackets" value="bracket" class="hidden peer">
-                    <label wire:click="setHasBracket(1)" for="bracket" class="p-2
-                    inline-flex items-center justify-between w-full text-gray-500 bg-white border border-gray-500 cursor-pointer
-                    peer-checked:border-blue-600 peer-checked:text-white  peer-checked:bg-blue-500 hover:bg-gray-100
-                    dark:bg-gray-800 dark:hover:bg-gray-700 rounded-l-lg
-                     ">
-                        <div class="w-full truncate">{{ __('Bracket') }}</div>
-                    </label>
+                    <x-radio wire:click="setHasBracket(1)" value="bracket" name="has_brackets"
+                             class="rounded-t-lg md:rounded-tr-none md:rounded-bl-lg">
+                        {{ __('Bracket') }}
+                    </x-radio>
                 </li>
                 <li>
-                    <input type="radio" id="playoff" name="has_brackets" value="playoff" class="hidden peer">
-                    <label wire:click="setHasBracket(0)" for="playoff" class="p-2
-                    inline-flex items-center justify-between w-full text-gray-500 bg-white border border-gray-500 cursor-pointer
-                    peer-checked:border-blue-600 peer-checked:text-white  peer-checked:bg-blue-500 hover:bg-gray-100
-                    dark:bg-gray-800 dark:hover:bg-gray-700 rounded-r-lg
-                     ">
-                        <div class="w-full truncate">{{ __('Playoff') }}</div>
-                    </label>
+                    <x-radio wire:click="setHasBracket(0)" value="playoff" name="has_brackets"
+                             class="rounded-b-lg md:rounded-bl-none md:rounded-tr-lg">
+                        {{ __('Playoff') }}
+                    </x-radio>
                 </li>
             </ul>
         </div>
 
         <div class="mb-4">
             @error('label') <p class="text-red-500">{{ $message }}</p> @enderror
-            <input
-                class="
-                border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full
-                shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
-                "
-                wire:model="label" name="label" id="label" type="text" placeholder="{{ __('Label') }}">
+            <x-input class="w-full"
+                     wire:model="label" name="label" id="label" type="text" placeholder="{{ __('Label') }}"></x-input>
         </div>
         <div class="mb-4">
             @error('date') <p class="text-red-500">{{ $message }}</p> @enderror
-            <input
-                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full
-                    shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                wire:model="date" name="date" id="date" type="date">
+            <x-input class="w-full"
+                     wire:model="date" name="date" id="date" type="date"></x-input>
         </div>
         <div class="mb-6">
             @error('place') <p class="text-red-500">{{ $message }}</p> @enderror
-            <input
-                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full
-                        shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                wire:model="place" name="place" id="place" type="text" placeholder="{{ __('Place') }}">
+            <x-input class="w-full"
+                     wire:model="place" name="place" id="place" type="text" placeholder="{{ __('Place') }}"></x-input>
         </div>
 
 
         <div class="flex items-center justify-end">
             <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                 {{ __('Create') }}
             </button>
         </div>
     </form>
+    <div class="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 grow">
+        <h1 class="text-xl font-bold mb-3 dark:text-white">{{ __('My tournaments') }}</h1>
+        <div class="flex gap-3 flex-wrap">
+            @forelse($tournaments->sortByDesc('date') as $tournament)
+                <a href="/tournaments/{{ $tournament->id }}/registration"
+                   class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-200">{{ $tournament->label }}</h5>
+                    <div class="font-normal text-gray-700 dark:text-gray-400">
+                        <p class="text-xs">{{ Carbon\Carbon::parse($tournament->date)->format('j M Y') }},
+                            <span>{{ $tournament->date == date('Y-m-d') ?
+                        __('Today') : Carbon\Carbon::parse($tournament->date)->diffForHumans() }}</span>
+                        </p>
+                        <p>{{ $tournament->place }}</p>
+                        <p>
+                            @if($tournament->team_size == 0)
+                                {{ __('Precision') }}
+                            @else
+                                @if($tournament->team_size == 1)
+                                    {{ __('Simple') }}
+                                @elseif($tournament->team_size == 2)
+                                    {{ __('Double') }}
+                                @elseif($tournament->team_size == 3)
+                                    {{ __('Triple') }}
+                                @endif
+                                -
+                                @if($tournament->has_brackets)
+                                    {{ __('Pools') }}
+                                @else
+                                    {{ __('Playoff') }}
+                                @endif
+                            @endif
+                        </p>
+                    </div>
+                </a>
+            @empty
+                <p class=" dark:text-gray-300">{{ __('No tournament registered') }}</p>
+            @endforelse
+        </div>
+    </div>
 </div>
